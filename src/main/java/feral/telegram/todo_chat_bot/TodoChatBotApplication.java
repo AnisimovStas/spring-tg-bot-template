75e9ae89a5b01fc1f308c2c68c4c1a0f68480a66
Stanislav <AnisimovSA@apheco.ru>
2024-07-10 11:49:48 +0400

    init java spring telegram bot template

---
+++ src/main/java/feral/telegram/todo_chat_bot/TodoChatBotApplication.java
@@ -0,0 +1,13 @@
+package feral.telegram.todo_chat_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoChatBotApplication.class, args);
+	}
+
+}
